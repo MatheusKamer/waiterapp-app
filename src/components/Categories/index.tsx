fb3bc2dd98cfb575e4f8e2982edbf0a1d5017c5f
Matheus Kamer <matheus_kamer@hotmail.com>
2025-03-12 19:43:28 -0400

    feat: enhance Categories component with selection functionality and touchable styles

--- src/components/Categories/index.tsx
+++ src/components/Categories/index.tsx
@@ -3,8 +3,16 @@ import { FlatList } from 'react-native';
 import { Text } from '../Text';
 import { Category, Icon } from './styles';
 import { categories } from '../../mocks/categories';
+import { useState } from 'react';
 
 export function Categories() {
+  const [selectedCategory, setSelectedCategory] = useState('');
+
+  function handleSelectCategory(categoryId: string) {
+    const category = selectedCategory === categoryId ? '' : categoryId;
+    setSelectedCategory(category);
+  }
+
   return (
     <FlatList
       horizontal
@@ -12,14 +20,18 @@ export function Categories() {
       showsHorizontalScrollIndicator={false}
       contentContainerStyle={{ paddingRight: 24 }}
       keyExtractor={(category) => category._id}
-      renderItem={({ item: category }) => (
-        <Category>
-          <Icon>
-            <Text>{category.icon}</Text>
-          </Icon>
-          <Text>{category.name}</Text>
-        </Category>
-      )}
+      renderItem={({ item: category }) => {
+        const isSelected = selectedCategory === category._id;
+
+        return (
+          <Category onPress={() => handleSelectCategory(category._id)}>
+            <Icon>
+              <Text opacity={isSelected ? 1 : 0.5}>{category.icon}</Text>
+            </Icon>
+            <Text opacity={isSelected ? 1 : 0.5}>{category.name}</Text>
+          </Category>
+        );
+      }}
     />
   );
 }
