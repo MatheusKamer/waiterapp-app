b25820352311710f997c306351dab49b839df1fe
Matheus Kamer <matheus_kamer@hotmail.com>
2025-03-22 14:22:36 -0400

    feat: integrate API calls for categories and products in Main component

--- src/Main/index.tsx
+++ src/Main/index.tsx
@@ -14,24 +14,34 @@ import { Header } from '../components/Header';
 import { Categories } from '../components/Categories';
 import { Button } from '../components/Button';
 import { TableModal } from '../components/TableModal';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Cart } from '../components/Cart';
 import { CartItem } from '../@types/CartItemType';
 import { Product } from '../@types/ProductType';
 
-import { products as mockProducts } from '../mocks/products';
-import { categories as mockCategories } from '../mocks/categories';
 import { Empty } from '../components/Icons/Empty';
 import { Text } from '../components/Text';
 import { Category } from '../@types/CategoryType';
+
+import { api } from '../utils/api';
 
 export function Main() {
   const [isTableModalVisible, setIsTableModalVisible] = useState(false);
   const [selectedTable, setSelectedTable] = useState('');
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
-  const [isLoading] = useState(false);
-  const [products] = useState<Product[]>(mockProducts);
-  const [categories] = useState<Category[]>(mockCategories);
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [categories, setCategories] = useState<Category[]>([]);
+
+  useEffect(() => {
+    Promise.all([api.get('/categories'), api.get('/products')]).then(
+      ([categoriesResponse, productsResponse]) => {
+        setCategories(categoriesResponse.data);
+        setProducts(productsResponse.data);
+        setIsLoading(false);
+      }
+    );
+  }, []);
 
   function handleOpenTableModal() {
     setIsTableModalVisible(true);
